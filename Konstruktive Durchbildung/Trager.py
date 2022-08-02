2461a06740f0097082fabfbc930d9a0bf4d31111
titatoubn99 <tobias.tabarelli@gmail.com>
2022-08-02 15:38:36 +0200

    festigkeitseigenschaften added

--- Konstruktive Durchbildung/Trager.py
+++ Konstruktive Durchbildung/Trager.py
@@ -2,14 +2,50 @@
 
 import math
 
+
 class Trager:
 
-    def __init__(self, id, breite, hohe, lange, fck, fyk) -> None:
+    """ festigkeitsKennwerte = {
+        "C12/15": { "fck": 12, "fctm": 1.6 },
+        "C16/20": { "fck": 16, "fctm": 1.9 },
+        "C20/25": { "fck": 20, "fctm": 2.2 },
+        "C25/30": { "fck": 25, "fctm": 2.6 },
+        "C30/37": { "fck": 30, "fctm": 2.9 },
+        "C35/45": { "fck": 35, "fctm": 3.2 },
+        "C40/50": { "fck": 40, "fctm": 3.5 },
+        "C45/55": { "fck": 45, "fctm": 3.8 },
+        "C50/60": { "fck": 50, "fctm": 4.1 },
+        "C55/67": { "fck": 55, "fctm": 4.2 },
+        "C60/75": { "fck": 60, "fctm": 4.4 },
+        "C70/85": { "fck": 70, "fctm": 4.6 },
+        "C80/95": { "fck": 80, "fctm": 4.8 },
+        "C90/105": { "fck": 90, "fctm": 5.0 }
+    } """
+
+    def __init__(self, id, breite, hohe, lange, betongute, fyk) -> None:
+        self.festigkeitsKennwerte = {
+            "C12/15": { "fck": 12, "fctm": 1.6 },
+            "C16/20": { "fck": 16, "fctm": 1.9 },
+            "C20/25": { "fck": 20, "fctm": 2.2 },
+            "C25/30": { "fck": 25, "fctm": 2.6 },
+            "C30/37": { "fck": 30, "fctm": 2.9 },
+            "C35/45": { "fck": 35, "fctm": 3.2 },
+            "C40/50": { "fck": 40, "fctm": 3.5 },
+            "C45/55": { "fck": 45, "fctm": 3.8 },
+            "C50/60": { "fck": 50, "fctm": 4.1 },
+            "C55/67": { "fck": 55, "fctm": 4.2 },
+            "C60/75": { "fck": 60, "fctm": 4.4 },
+            "C70/85": { "fck": 70, "fctm": 4.6 },
+            "C80/95": { "fck": 80, "fctm": 4.8 },
+            "C90/105": { "fck": 90, "fctm": 5.0 }
+        }
         self.id = id
         self.breite = breite
         self.hohe = hohe
         self.lange = lange
-        self.fck = fck
+        self.betongute = betongute
+        self.fck = self.festigkeitsKennwerte[self.betongute]["fck"]
+        self.fctm = self.festigkeitsKennwerte[self.betongute]["fctm"]
         self.fyk = fyk
 
 
@@ -23,8 +59,8 @@ class Trager:
 
 class Balken(Trager):
 
-    def __init__(self, id, breite, hohe, lange, fck, fyk) -> None:
-        super().__init__(id, breite, hohe, lange, fck, fyk)
+    def __init__(self, id, breite, hohe, lange, betongute, fyk) -> None:
+        super().__init__(id, breite, hohe, lange, betongute, fyk)
 
 
 
@@ -39,6 +75,9 @@ class Wande(Trager):
 
 
 if __name__ == "__main__":
-    # X1 = Balken()
+    # Beispielbalken mit 1
+    X1 = Balken(1, 1, 1, 1, "C40/50", 550)
+    print(type(X1.betongute))
+    print(X1.fctm)
     pass
     
