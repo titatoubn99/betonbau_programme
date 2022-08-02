b9f22bef1de4c7b48d0826301574f940a42ce701
titatoubn99 <tobias.tabarelli@gmail.com>
2022-08-02 10:04:40 +0200

    last one didnt worked out

--- Konstruktive Durchbildung/Trager.py
+++ Konstruktive Durchbildung/Trager.py
@@ -1,1 +1,13 @@
-#!python 
+#!/usr/bin/env python3
+
+class Trager:
+
+    def __init__(self, id, breite, hohe, lange, fck, fyk) -> None:
+        self.id = id
+        self.breite = breite
+        self.hohe = hohe
+        self.lange = lange
+        self.fck = fck
+        self.fyk = fyk
+
+    
