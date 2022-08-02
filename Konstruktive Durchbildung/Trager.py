72166bdcf1e42e98fd5468801024d1c8314fe7c8
titatoubn99 <tobias.tabarelli@gmail.com>
2022-08-02 10:59:49 +0200

    subclasses

--- Konstruktive Durchbildung/Trager.py
+++ Konstruktive Durchbildung/Trager.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import math
+
 class Trager:
 
     def __init__(self, id, breite, hohe, lange, fck, fyk) -> None:
@@ -10,4 +12,33 @@ class Trager:
         self.fck = fck
         self.fyk = fyk
 
+
+    def querschnittsEigenschaften(self):
+        querschnittsFlache = self.breite * self.hohe
+        volumen = self.breite * self.hohe * self.lange
+        tragheitsmoment = self.breite * math.pow(self.hohe, 3) / 12
+        widerstandsmoment = self.breite * math.pow(self.hohe, 2) / 6
+        return querschnittsFlache, volumen, tragheitsmoment, widerstandsmoment
+
+
+class Balken(Trager):
+
+    def __init__(self, id, breite, hohe, lange, fck, fyk) -> None:
+        super().__init__(id, breite, hohe, lange, fck, fyk)
+
+
+
+class Platten(Balken):
+    pass
+
+class Stutzen(Trager):
+    pass
+
+class Wande(Trager):
+    pass
+
+
+if __name__ == "__main__":
+    # X1 = Balken()
+    pass
     
